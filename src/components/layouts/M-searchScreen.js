ad8e9e6541c63349fe0e1d215d2f1c0c1e4fed3e
Abolfazl <iabolfazl83@yahoo.com>
2023-11-06 01:26:13 +0330

    added MobileSearchScreen.js layout.

---
+++ src/components/layouts/M-searchScreen.js
@@ -0,0 +1,93 @@
+import React, {useRef, useState} from 'react';
+
+function MobileSearchScreen({screenSearch, setScreenSearch}) {
+    const searchInput = useRef(null)
+    return (
+        <div
+            className={`search-screen-container min-vw-100 position-fixed ${screenSearch === true ? `top-0` : `top-100`} container py-3 px-4`}>
+            <div className="mobile-search-container d-flex py-2 align-items-center">
+                <div className="close-search col-1">
+                    <button className="btn p-0 m-0 border-0 d-flex" onClick={() => setScreenSearch(false)}>
+                        <i className="fa-solid fa-arrow-right"></i>
+                    </button>
+                </div>
+                <div className="mobile-search col-10 d-flex justify-content-start">
+                    <input ref={searchInput} className="bg-white border-0 w-100 mx-3 d-flex align-items-center"
+                           type="text"
+                           placeholder="جستجو..."/>
+                </div>
+                <div className="clear-search col-1">
+                    <span onClick={() => searchInput.current.value = ''}>
+                    <i className="fa-solid fa-circle-xmark"></i>
+                    </span>
+                </div>
+            </div>
+
+            <div className="search-options-container">
+                <div className="recently-searched d-flex justify-content-start my-3">
+                    <i className="fa-solid fa-clock-rotate-left col-1"></i>
+                    <span className="col-11"> آخرین جستجوهای شما</span>
+                </div>
+                <div className="recently-searched-items d-flex overflow-y-hidden overflow-x-auto hide-scrollbar">
+                    <a href="" className="recently-searched-item d-flex align-items-center mx-1"> ساعت
+                        رومیزی <i
+                            className="fa-solid fa-angle-left mx-1"></i></a>
+                    <a href="" className="recently-searched-item d-flex align-items-center mx-1"> ساعت
+                        رومیزی <i
+                            className="fa-solid fa-angle-left mx-1"></i></a>
+                    <a href="" className="recently-searched-item d-flex align-items-center mx-1"> ساعت
+                        رومیزی <i
+                            className="fa-solid fa-angle-left mx-1"></i></a>
+                    <a href="" className="recently-searched-item d-flex align-items-center mx-1"> ساعت
+                        رومیزی <i
+                            className="fa-solid fa-angle-left mx-1"></i></a>
+                    <a href="" className="recently-searched-item d-flex align-items-center mx-1"> ساعت
+                        رومیزی <i
+                            className="fa-solid fa-angle-left mx-1"></i></a>
+                    <a href="" className="recently-searched-item d-flex align-items-center mx-1"> ساعت
+                        رومیزی <i
+                            className="fa-solid fa-angle-left mx-1"></i></a>
+                </div>
+
+                <div className="popular-searches d-flex justify-content-start my-3">
+                    <i className="fa-solid fa-fire-flame-curved col-1"></i>
+                    <span className="col-10">جستجوهای پرطرفدار</span>
+                </div>
+                <div
+                    className="poplar-searches-items d-flex gap flex-nowrap overflow-x-auto overflow-y-hidden hide-scrollbar">
+                    <a href="" className="recently-searched-item d-flex align-items-center mx-1">گوی موزیکال<i
+                        className="fa-solid fa-angle-left mx-1"></i></a> <a href=""
+                                                                            className="recently-searched-item d-flex align-items-center mx-1">گوی
+                    موزیکال<i
+                        className="fa-solid fa-angle-left mx-1"></i></a> <a href=""
+                                                                            className="recently-searched-item d-flex align-items-center mx-1">گوی
+                    موزیکال<i
+                        className="fa-solid fa-angle-left mx-1"></i></a> <a href=""
+                                                                            className="recently-searched-item d-flex align-items-center mx-1">گوی
+                    موزیکال<i
+                        className="fa-solid fa-angle-left mx-1"></i></a> <a href=""
+                                                                            className="recently-searched-item d-flex align-items-center mx-1">گوی
+                    موزیکال<i
+                        className="fa-solid fa-angle-left mx-1"></i></a> <a href=""
+                                                                            className="recently-searched-item d-flex align-items-center mx-1">گوی
+                    موزیکال<i
+                        className="fa-solid fa-angle-left mx-1"></i></a> <a href=""
+                                                                            className="recently-searched-item d-flex align-items-center mx-1">گوی
+                    موزیکال<i
+                        className="fa-solid fa-angle-left mx-1"></i></a> <a href=""
+                                                                            className="recently-searched-item d-flex align-items-center mx-1">گوی
+                    موزیکال<i
+                        className="fa-solid fa-angle-left mx-1"></i></a> <a href=""
+                                                                            className="recently-searched-item d-flex align-items-center mx-1">گوی
+                    موزیکال<i
+                        className="fa-solid fa-angle-left mx-1"></i></a> <a href=""
+                                                                            className="recently-searched-item d-flex align-items-center mx-1">گوی
+                    موزیکال<i
+                        className="fa-solid fa-angle-left mx-1"></i></a>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default MobileSearchScreen;
