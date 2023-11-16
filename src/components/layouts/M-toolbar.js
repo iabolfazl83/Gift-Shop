c574787c10217e69c2aafc04757d683740b30148
Abolfazl <iabolfazl83@yahoo.com>
2023-11-16 18:54:12 +0330

    refactored import.

--- src/components/layouts/M-toolbar.js
+++ src/components/layouts/M-toolbar.js
@@ -1,23 +1,36 @@
 import React from "react";
+import {toFarsiNumber} from "../../services/numberToPersian";
 
 function MobileToolbar() {
+
     return (
-        <div className="mobile-toolbar-menu d-flex justify-content-between p-2">
-            <div className="mobile-toolbar-menu-item"><a href=""><i
-                className="fa-solid fa-shop d-block text-center mobile-toolbar-menu-icons"></i>فروشگاه</a></div>
-            <div className="mobile-toolbar-menu-item"><a href=""><i
-                className="fa-solid fa-heart d-block text-center mobile-toolbar-menu-icons"></i>علاقه مندی</a>
-            </div>
-            <div className="mobile-toolbar-menu-item"><a href=""><i
-                className="fa-solid fa-cart-shopping d-block text-center mobile-toolbar-menu-icons"></i>سبد خرید</a>
+        <div className="mobile-toolbar-menu box-shadow d-flex justify-content-between px-3 overflow-x-auto">
+            <div className="mobile-toolbar-menu-item">
+                <a href="">
+                    <i className="fa-solid fa-shop d-block text-center mobile-toolbar-menu-icons"></i>
+                    فروشگاه
+                </a>
             </div>
             <div className="mobile-toolbar-menu-item">
-                <a href=""><i
-                    className="fa-solid fa-user d-block text-center mobile-toolbar-menu-icons"></i>حساب کاربری
-                    من</a>
+                <a href="">
+                    <i className="fa-solid fa-heart d-block text-center mobile-toolbar-menu-icons position-relative"><span className="badge-icon">{toFarsiNumber(0)}</span></i>
+                    علاقه مندی
+                </a>
+            </div>
+            <div className="mobile-toolbar-menu-item">
+                <a href="">
+                    <i className="fa-solid fa-cart-shopping d-block text-center mobile-toolbar-menu-icons position-relative"><span className="badge-icon">{toFarsiNumber(9)}</span></i>
+                    سبد خرید
+                </a>
+            </div>
+            <div className="mobile-toolbar-menu-item">
+                <a href="">
+                    <i className="fa-solid fa-user d-block text-center mobile-toolbar-menu-icons"></i>
+                    حساب کاربری من
+                </a>
             </div>
         </div>
-    )
+    );
 }
 
 export default MobileToolbar;
