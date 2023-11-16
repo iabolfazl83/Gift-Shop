ae90efc437edb0830479c766653075d886f8f159
Abolfazl <iabolfazl83@yahoo.com>
2023-11-16 18:46:35 +0330

    created index file, refactored imports.

---
+++ src/components/index.js
@@ -0,0 +1,11 @@
+// Layout Components
+
+export {default as Main} from "./layouts/Main";
+export {default as MobileToolbar} from "./layouts/M-toolbar";
+export {default as MobileSearchScreen} from "./layouts/M-searchScreen";
+export {default as Header} from "./layouts/Header";
+export {default as Footer} from "./layouts/Footer";
+
+// Other Components
+
+export {default as LoadingScreen} from "./LoadingScreen";
