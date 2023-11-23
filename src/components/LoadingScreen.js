77820af32b5adc90142d5011bbfef59901b2246f
Abolfazl <iabolfazl83@yahoo.com>
2023-11-24 01:21:16 +0330

    updated LoadingScreen.js css.

--- src/components/LoadingScreen.js
+++ src/components/LoadingScreen.js
@@ -1,12 +1,27 @@
 const LoadingScreen = () => {
     return (
         <>
-            <img
-                src={require("../assets/loading-gif.gif")}
-                className="d-block m-auto p-4 loading-screen"
-                style={{width: "100px"}}
-                alt="loading"
-            />
+            <div style={{
+                position: "fixed",
+                width: "100%",
+                height: "100%",
+                top: "0",
+                bottom: "0",
+                left: "0",
+                right: "0",
+                backgroundColor: "rgba(255,255,251,.97)",
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center,",
+                zIndex: "999999",
+            }}>
+                <img
+                    src={require("../assets/loading-gif.gif")}
+                    className="d-block m-auto p-4 loading-screen"
+                    style={{width: "100px"}}
+                    alt="loading"
+                />
+            </div>
         </>
     );
 };
