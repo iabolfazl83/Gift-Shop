17bbbed66a435b1f95a6fad72e57b908973abb0d
Abolfazl <iabolfazl83@yahoo.com>
2023-11-25 13:45:59 +0330

    fixed search section buttons.

--- src/components/layouts/M-searchScreen.js
+++ src/components/layouts/M-searchScreen.js
@@ -1,4 +1,4 @@
-import React, {useRef, useState} from 'react';
+import React, {useRef} from 'react';
 
 function MobileSearchScreen({screenSearch, setScreenSearch}) {
     const searchInput = useRef(null)
