5d4c47e4c1ef87c9b9738fcd0bd72509e342010e
Abolfazl <iabolfazl83@yahoo.com>
2023-12-02 18:29:37 +0330

    added new Components to export.

--- src/components/index.js
+++ src/components/index.js
@@ -3,9 +3,13 @@
 export {default as Main} from "./layouts/Main";
 export {default as MobileToolbar} from "./layouts/M-toolbar";
 export {default as MobileSearchScreen} from "./layouts/M-searchScreen";
-export {default as Header} from "./layouts/Header";
+export {default as Header} from "./layouts/Header/Header";
+export {default as Navbar} from "./layouts/Header/Navbar";
 export {default as Footer} from "./layouts/Footer";
 
-// Other Components
+// Other Pages Components
+export {default as ContactUs} from "./ContactUs";
+export {default as GiftShopMag} from "./GiftShopMag";
 
+// Other Components
 export {default as LoadingScreen} from "./LoadingScreen";
