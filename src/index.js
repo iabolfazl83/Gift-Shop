18189e26ef9ab482def304c99ee1946c794b4f54
Abolfazl <iabolfazl83@yahoo.com>
2023-12-02 18:31:27 +0330

    add React Router DOM for routing support.

--- src/index.js
+++ src/index.js
@@ -1,15 +1,39 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {BrowserRouter, Routes, Route, Link} from "react-router-dom";
 import App from './App';
 import './index.css';
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css"
 import "bootstrap/dist/js/bootstrap.min.js"
 import "./media-queries.css"
+import {ContactUs} from "./components/index";
+import GiftShopMag from "./components/GiftShopMag";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
-        <App/>
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<App/>}></Route>
+                <Route path="/contact-us" element={<ContactUs/>}/>
+                <Route path="/giftshop-mag" element={<GiftShopMag/>}/>
+                <Route path="*" element={
+                    <main className="container m-auto">
+                        <div className="container w-auto d-flex flex-column gap-5 align-items-center">
+                            <div className="w-25">
+                                <img className="w-100" src={require("./assets/images/download.png")} alt="404 error"/>
+                            </div>
+                            <div className="fs-5 text-404">این راه به جایی نمی‌رسه!</div>
+                            <div>
+                                <p className="text-404 text-center">به نظر آدرس را اشتباه وارد کرده‌اید.</p>
+                                <p className="text-404 text-center">برای پیدا کردن مسیر درست می‌توانید <Link
+                                    className="active-page fw-bolder" to={"/"}>به خانه بازگردید</Link>.</p>
+                            </div>
+                        </div>
+                    </main>
+                }/>
+            </Routes>
+        </BrowserRouter>
     </React.StrictMode>
 );
