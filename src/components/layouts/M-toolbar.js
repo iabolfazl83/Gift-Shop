355d229c69393e14c2a3bb6eb756e2af57a745ad
Abolfazl <iabolfazl83@yahoo.com>
2023-12-13 11:49:21 +0330

    added Link to Store.js.

--- src/components/layouts/M-toolbar.js
+++ src/components/layouts/M-toolbar.js
@@ -1,25 +1,26 @@
 import React from "react";
-import {toFarsiNumber} from "../../services/numberToPersian";
+import {Link} from "react-router-dom";
+import {numberToPersian} from "../../services/numberToPersian";
 
 function MobileToolbar() {
 
     return (
         <div className="mobile-toolbar-menu box-shadow d-flex justify-content-between px-3 overflow-x-auto">
             <div className="mobile-toolbar-menu-item">
-                <a href="">
+                <Link to="/store">
                     <i className="fa-solid fa-shop d-block text-center mobile-toolbar-menu-icons"></i>
                     فروشگاه
-                </a>
+                </Link>
             </div>
             <div className="mobile-toolbar-menu-item">
                 <a href="">
-                    <i className="fa-solid fa-heart d-block text-center mobile-toolbar-menu-icons position-relative"><span className="badge-icon">{toFarsiNumber(0)}</span></i>
+                    <i className="fa-solid fa-heart d-block text-center mobile-toolbar-menu-icons position-relative"><span className="badge-icon">{numberToPersian(0)}</span></i>
                     علاقه مندی
                 </a>
             </div>
             <div className="mobile-toolbar-menu-item">
                 <a href="">
-                    <i className="fa-solid fa-cart-shopping d-block text-center mobile-toolbar-menu-icons position-relative"><span className="badge-icon">{toFarsiNumber(9)}</span></i>
+                    <i className="fa-solid fa-cart-shopping d-block text-center mobile-toolbar-menu-icons position-relative"><span className="badge-icon">{numberToPersian(8)}</span></i>
                     سبد خرید
                 </a>
             </div>
