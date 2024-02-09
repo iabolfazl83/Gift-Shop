4a4aeb76816646be412676c2a14d30108c7e968d
Abolfazl <iabolfazl83@yahoo.com>
2024-02-10 00:41:49 +0330

    added auto comma insert for prices.

--- src/services/numberToPersian.js
+++ src/services/numberToPersian.js
@@ -1,7 +1,20 @@
+export function insertRialComma(n) {
+    let m = "";
+    for (let i = 0; i < n.length; i++) {
+        let c = n.substr(n.length - i - 1, 1);
+        if (i % 3 == 0 & i > 0) {
+            m = c + ',' + m;
+        } else {
+            m = c + m;
+        }
+    }
+    return m;
+}
+
 export function numberToPersian(n) {
     const farsiDigits = ['۰', '۱', '۲', '۳', '۴', '۵', '۶', '۷', '۸', '۹'];
 
     return n
         .toString()
-        .replace(/\d/g, x => farsiDigits[x]);
+        .replace(/\d/g, x => farsiDigits[x])
 }
